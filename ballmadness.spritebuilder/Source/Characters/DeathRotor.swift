b6aa800613345eccab64a56b1e3940cf0674a8b9
Martin Walsh <martin.walsh@gmail.com>
2015-07-11 21:24:43 -0700

    Add DeathRotor Class

---
+++ ballmadness.spritebuilder/Source/Characters/DeathRotor.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+class DeathRotor : Death {
+    
+    override func didLoadFromCCB() {
+        super.didLoadFromCCB()
+        
+        // Rotation
+        var rotation = CCActionRotateBy(duration: 1.0,angle: 360)
+        body.runAction(CCActionRepeatForever(action:rotation))
+        
+        // Movement
+        var movementOne = CCActionMoveBy(duration: 2.0, position: ccp(-568,0))
+        var sequence    = CCActionSequence(array: [movementOne,movementOne.reverse()])
+        
+        body.runAction(CCActionRepeatForever(action:sequence))
+    }
+}
